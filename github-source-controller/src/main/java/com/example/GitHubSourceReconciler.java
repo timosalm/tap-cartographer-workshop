d2bd95a28a8606aad6f92136d4aa9dc6efa51ec7
Timo Salm <tsalm@vmware.com>
2022-06-05 20:10:02 +0200

    first commit

--- github-source-controller/src/main/java/com/example/GitHubSourceReconciler.java
+++ github-source-controller/src/main/java/com/example/GitHubSourceReconciler.java
@@ -4,6 +4,7 @@ import com.example.model.GitHubEvent;
 import com.example.model.V1GitHubRepository;
 import com.example.model.V1GitHubRepositoryList;
 import io.kubernetes.client.apimachinery.GroupVersion;
+import io.kubernetes.client.common.KubernetesListObject;
 import io.kubernetes.client.extended.controller.reconciler.Reconciler;
 import io.kubernetes.client.extended.controller.reconciler.Request;
 import io.kubernetes.client.extended.controller.reconciler.Result;
@@ -34,15 +35,18 @@ public class GitHubSourceReconciler implements Reconciler {
         Lister<V1GitHubRepository> lister = new Lister<>(informer.getIndexer(), request.getNamespace());
         V1GitHubRepository resource = lister.get(request.getName());
 
+        log.info("triggered reconciling " + resource.getMetadata().getNamespace() + "/" + resource.getMetadata().getName());
+
         if (resource == null || resource.getMetadata().getDeletionTimestamp() != null) {
             return new Result(false);
         }
 
         GitHubEvent latestGithubEvent = service.getLatestGithubEvent(resource.getSpec().getUrl(), resource.getSpec().getBranch());
         resource.updateStatus(latestGithubEvent.getTarballUrl(), latestGithubEvent.getRevision());
+        log.info("Trying to update status for " + resource.getMetadata().getNamespace() + "/" + resource.getMetadata().getName() + " with " + latestGithubEvent.getTarballUrl());
 
         GroupVersion gv = GroupVersion.parse(resource);
-        GenericKubernetesApi<V1GitHubRepository, ?> status = new GenericKubernetesApi<>(V1GitHubRepository.class, V1GitHubRepositoryList.class,
+        GenericKubernetesApi<V1GitHubRepository, ?> status = new GenericKubernetesApi<>(V1GitHubRepository.class, KubernetesListObject.class,
                 gv.getGroup(), gv.getVersion(), "githubrepositories", this.apiClient);
 
         KubernetesApiResponse<V1GitHubRepository> update = status.updateStatus(resource, V1GitHubRepository::getStatus);
