b9aaeb423f345e2b4466166b5c64e8d07e32d7f3
Ubuntu <ubuntu@ip-10-0-131-128.us-west-2.compute.internal>
2022-10-18 22:41:52 +0000

    changing image registries + minor tweaks

--- workshop/imgbuild.sh
+++ workshop/imgbuild.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -ex
+
+IMAGE_REPO=harbor.services.demo.jg-aws.com
+
+# echo "Reinitialize password manager"
+# pass show docker-credential-helpers/docker-pass-initialized-check
+
+# echo "Docker login"
+# # docker login https://harbor.services.demo.jg-aws.com --username tap-workshop --password ''
+
 echo "Building workshop image"
-docker build . -t harbor.svcs.az-tkglab.sprok8s.com/tap-workshop/cartographer-workshop
+docker build . -t $IMAGE_REPO/tap-workshop/cartographer-workshop
 
 echo "Pushing the workshop image"
-docker push harbor.svcs.az-tkglab.sprok8s.com/tap-workshop/cartographer-workshop:latest
+docker push $IMAGE_REPO/tap-workshop/cartographer-workshop:latest
--- workshop/install.sh
+++ workshop/install.sh
@@ -1,4 +1,9 @@
-set -x
+#!/usr/bin/env bash
+set -xe
 
-ytt template -f resources -f values.yaml --ignore-unknown-comments | kapp deploy -n tap-install -a cartographer-workshops -f- --diff-changes --yes
+echo "Deploying the workshop"
+ytt template -f resources -f values.yaml --ignore-unknown-comments \
+| kapp deploy -n tap-install -a cartographer-workshops -f- --diff-changes --yes
+
+echo "Removing the learning center pod"
 kubectl delete pod -l deployment=learningcenter-operator -n learningcenter
