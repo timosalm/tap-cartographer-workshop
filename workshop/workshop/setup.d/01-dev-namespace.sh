fb72352b9bde6de3cb4df4f0f82f4db96f614ef6
Ubuntu <ubuntu@ip-10-0-131-128.us-west-2.compute.internal>
2022-10-27 21:47:40 +0000

    Introducing new Custom Supply Chain

--- workshop/workshop/setup.d/01-dev-namespace.sh
+++ workshop/workshop/setup.d/01-dev-namespace.sh
@@ -44,37 +44,49 @@ EOF
 
 # Create a custom Tekton Pipeline to for code scanning
 cat << \EOF | kubectl apply -f -
-apiVersion: tekton.dev/v1beta1
-kind: Pipeline
+apiVersion: scanning.apps.tanzu.vmware.com/v1beta1
+kind: ScanPolicy
 metadata:
-  name: developer-defined-tekton-pipeline
-  labels:
-    apps.tanzu.vmware.com/pipeline: test     # (!) required
+  name: lax-scan-policy
 spec:
-  params:
-    - name: source-url                       # (!) required
-    - name: source-revision                  # (!) required
-  tasks:
-    - name: test
-      params:
-        - name: source-url
-          value: $(params.source-url)
-        - name: source-revision
-          value: $(params.source-revision)
-      taskSpec:
-        params:
-          - name: source-url
-          - name: source-revision
-        steps:
-          - name: test
-            image: maven:3-openjdk-11
-            script: |-
-              cd `mktemp -d`
-              wget -qO- $(params.source-url) | tar xvz -m
-              mvn test
+  regoFile: |
+    package main
+
+    # Accepted Values: "Critical", "High", "Medium", "Low", "Negligible", "UnknownSeverity"
+    notAllowedSeverities := ["UnknownSeverity"]
+
+    ignoreCves := []
+
+    contains(array, elem) = true {
+      array[_] = elem
+    } else = false { true }
+
+    isSafe(match) {
+      severities := { e | e := match.ratings.rating.severity } | { e | e := match.ratings.rating[_].severity }
+      some i
+      fails := contains(notAllowedSeverities, severities[i])
+      not fails
+    }
+
+    isSafe(match) {
+      ignore := contains(ignoreCves, match.id)
+      ignore
+    }
+
+    deny[msg] {
+      comps := { e | e := input.bom.components.component } | { e | e := input.bom.components.component[_] }
+      some i
+      comp := comps[i]
+      vulns := { e | e := comp.vulnerabilities.vulnerability } | { e | e := comp.vulnerabilities.vulnerability[_] }
+      some j
+      vuln := vulns[j]
+      ratings := { e | e := vuln.ratings.rating.severity } | { e | e := vuln.ratings.rating[_].severity }
+      not isSafe(vuln)
+      msg = sprintf("CVE %s %s %s", [comp.name, vuln.id, ratings])
+    }
 EOF
 
-# Apply Scan Policies to the session workshop
+# Apply strict scan policy to the session workshop
 cat << EOF | kubectl apply -f -
 apiVersion: scanning.apps.tanzu.vmware.com/v1beta1
 kind: ScanPolicy
